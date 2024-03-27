aac250898bd58bacb0a29edd037ce4d1055231af
houdini91 <mdstrauss91@gmail.com>
2024-03-27 10:23:07 +0200

    fix bump

--- bump-version.sh
+++ bump-version.sh
@@ -4,8 +4,8 @@ set -ex
 version=$1
 git checkout dev
 git pull origin dev
+git branch -D  prep_release/$version || true
 git checkout -b prep_release/$version || git checkout prep_release/$version || true
-git checkout -b prep_release/$version
 sed -i "s/dev-latest/$version/g" action.yml
 git add action.yml || true
 git commit -m "bump version" || true
